6ba3441bc8f0ca86dea7d7e1e777c9ae1256e62b
akshaydavkare2021 <ardavkare@gmail.com>
2024-06-26 19:53:30 +0530

    new branch made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, HttpClientModule],
+  
+
 })
 export class AppComponent {
   title = 'first-app';
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -7,22 +7,26 @@ import { UsersDataService } from '../services/users-data.service';
   imports: [],
   templateUrl: './child.component.html',
   styleUrl: './child.component.css',
+  providers: [UsersDataService]
 })
 export class ChildComponent implements OnInit {
   @Input() childInput: any;
-  @Output() updateDataEvent = new EventEmitter<string>();
+  @Output() updateDataEvent = new EventEmitter<string>();   
   constructor(public userService: UsersDataService) {}
-  studentData: any = {
-    address: 'Asd 12 street',
-    pin: '235412',
-  };
+  // studentData: any = {
+  //   address: 'Asd 12 street',
+  //   pin: '235412',
+  // };
   user: any;
   ngOnInit(): void {
     console.log(this.childInput);
-    this.user = this.userService.getUsers();
+    this.userService.getUsers().subscribe((res: any)=>{
+      console.log(res);
+      this.user = res
+    });
     console.log('user', this.user);
   }
-  sendData() {
-    this.updateDataEvent.emit(this.studentData);
-  }
+//   sendData() {
+//     this.updateDataEvent.emit(this.studentData);
+//   }
 }
